7bccf2e94bf4052351f2e174911ab70471265a50
Steve-PC\Steve <stermes66@gmail.com>
2017-07-02 11:25:51 -0400

    Shader and shader program .h and .cpp have been completed and uploaded

---
+++ Engine/ShaderProgram.h
@@ -0,0 +1,30 @@
+#ifndef SHADERPROGRAM_H
+#define SHADERPROGRAM_H
+
+#include <glad/glad.h>
+#include <string>
+
+class Shader;
+
+class ShaderProgram
+{
+public:
+	ShaderProgram(Shader* vertexShader, Shader* fragmentShader, Shader* geometryShader = NULL);
+	~ShaderProgram();
+
+	// Sets this shader program to be used
+	void Use();
+	unsigned int GetID();
+
+	// Sets the uniforms for the shader program
+	void SetBool(const std::string &name, bool value) const;
+	void SetInt(const std::string &name, int value) const;
+	void SetFloat(const std::string &name, float value) const;
+
+private:
+	unsigned int m_ID;
+	// compile error check
+	void CheckCompileErrors();
+};
+
+#endif 
--- Engine/Source.cpp
+++ Engine/Source.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#include "Shader.h"
+#include "ShaderProgram.h"
+
 void FrameBufferSizeCallBack(GLFWwindow* window, int width, int height);
 void HandleInput(GLFWwindow* window);
 // settings
@@ -34,27 +37,51 @@ int main()
 		return -1;
 	}
 
+
+
+	Shader* vShader = new Shader("BasicVertex.VS", GL_VERTEX_SHADER);
+	Shader* fShader = new Shader("BasicFragment.FS", GL_FRAGMENT_SHADER);
+
+	ShaderProgram* shaderProgram = new ShaderProgram(vShader, fShader);
+
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.0f,  0.5f, 0.0f
 	};
 
-	unsigned int VBO;
+	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
 		HandleInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);		
+
+		shaderProgram->Use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
 
+	delete vShader;
+	delete fShader;
 	glfwTerminate();
 	return 0;
 }
